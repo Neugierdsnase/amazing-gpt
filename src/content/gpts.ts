9f3281ba3d750d06d989b8a897061ee75814c41f
Konstantin Kovar <mail@vomkonstant.in>
2023-11-23 12:02:15 +0100

    added 11 new GPTs from Matt Wolfe's sheet

--- src/content/gpts.ts
+++ src/content/gpts.ts
@@ -254,5 +254,184 @@ export const gpts: GPTInfoType[] = [
 		url: 'https://chat.openai.com/g/g-tcmMldCYy-simpsonize-me',
 		image:
 			'https://files.oaiusercontent.com/file-17fHH5xrODVwm3MWDraASFA3?se=2123-10-14T18%3A36%3A02Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Da722ea47-dbe5-48e9-8edb-18b06967b0a3.png&sig=1B5F0S7gRjMV2HmqIaIqmiSwoiePyILURO7nm3KDTIs%3D'
+	},
+
+	{
+		id: isUuid('173b8815-e0aa-4086-af40-f0f77faf9bb3'),
+		name: 'GPTs Matchmaker',
+		author: {
+			name: 'SynLabs',
+			url: 'https://synlabs.pro'
+		},
+		description: 'Expert in matching users with suitable GPTs.',
+		tags: ['gpt', 'indexing', 'searching', 'meta'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-PCGBO2DdD-gpts-matchmaker',
+		image:
+			'https://files.oaiusercontent.com/file-aBomEOTt7zbEFziQobe33wwY?se=2123-10-21T12%3A23%3A14Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Df2b293cc-a024-4df7-9cf7-c421bc94ffe4.png&sig=E0JIEy09ZmfT2xsQvHLaD/ewYdn3IlYmFEFSTYZ%2BR8A%3D'
+	},
+
+	{
+		id: isUuid('aa7e86a1-3d2f-4fe8-b9a7-22fbe50f0d79'),
+		name: '/Imagine Anything',
+		author: {
+			name: 'Joshua Bodnar',
+			url: ''
+		},
+		description: 'You can create almost any image, making a masterpiece from thin air!',
+		tags: ['images', 'pictures', 'art'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-z9mT0nYIk-imagine-anything',
+		image:
+			'https://files.oaiusercontent.com/file-LK85nAOSowLnp8XUC8gdvPRf?se=2123-10-22T23%3A56%3A55Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D34.png&sig=Ug0eoGNweed8xhIkA3CYU2HXVltPBk0FHKEbXe/fXCg%3D'
+	},
+
+	{
+		id: isUuid('fd9688d6-60f1-49d5-b58e-b102018848f4'),
+		name: 'L6 Helix Sound Designer',
+		author: {
+			name: 'Joel P Gouker',
+			url: ''
+		},
+		description:
+			'I help you with Line 6 Helix sound design, focusing on custom patches and guitar tone guidance - V 0.3',
+		tags: ['music', 'guitar', 'sound design', 'audio', 'tone'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-jKUagForu-l6-helix-sound-designer',
+		image:
+			'https://files.oaiusercontent.com/file-4PrjMaqZ9TlAdXxcThPyjr2S?se=2123-10-17T02%3A04%3A22Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dc0225314-3cf6-4bb5-99c3-7908329b6703.png&sig=HDQHyu6cNPKdGbo68fIVLIeWrKAk3Ske2dh80C0tl5s%3D'
+	},
+
+	{
+		id: isUuid('47af4fb5-78c2-4288-86a9-4f305102b515'),
+		name: 'Dispatch Trainer',
+		author: {
+			name: '',
+			url: ''
+		},
+		description: 'Simulates 911 calls for dispatcher training.',
+		tags: ['911', 'dispatcher', 'emergency'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-JjYTt8d1p-dispatch-trainer',
+		image:
+			'https://files.oaiusercontent.com/file-4JeBm1luXC4B73EBKIrFs1oZ?se=2123-10-21T16%3A48%3A36Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3D4f290fcd-80d9-42f5-a892-8124c742e61b.png&sig=eIJUb5sOsWD/6H7AEVEJ3%2B0EKHIyo4Z5wHS/e9OWPqQ%3D'
+	},
+
+	{
+		id: isUuid('61f914d1-1a35-455f-886f-662b19d3dfbb'),
+		name: 'Portrait Cartoonator',
+		author: {
+			name: '',
+			url: 'rsugrafx.com'
+		},
+		description:
+			'Turns your photos into cartoon-style portraits. Just upload a photo, and the app automatically starts.',
+		tags: ['cartoon', 'portrait', 'image editing', 'pictures', 'photos', 'images'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-BVAcMP7wX-portrait-cartoonator',
+		image:
+			'https://files.oaiusercontent.com/file-W9AKvihAeIk1syTMYsC1aC9u?se=2123-10-21T09%3A34%3A51Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dlogo.png&sig=Nn9ZWpbtFwyhq1nlfFdQjwD21DhppL3No7VRZFoWF8Y%3D'
+	},
+
+	{
+		id: isUuid('bb2a68d2-9f7f-4ae0-bf26-9e925fa1b9ce'),
+		name: 'Pawtrait Creator',
+		author: {
+			name: '',
+			url: 'rsugrafx.com'
+		},
+		description:
+			'Creates cartoon pet portraits. Upload a photo of your pet, type its name, submit it, and watch the magic happen.',
+		tags: ['cartoon', 'portrait', 'image editing', 'pictures', 'photos', 'images'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-WYlcscE2T-pawtrait-creator',
+		image:
+			'https://files.oaiusercontent.com/file-YdiwHH35WY3pfNnmUWCeil7g?se=2123-10-21T09%3A37%3A53Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dlogo.png&sig=5XMhernnJp0cGH7%2BHMFnROC/g7lZHiXjGjc2qZN8kdo%3D'
+	},
+
+	{
+		id: isUuid('5213dba9-2ee8-4bf8-bf99-0d78442b8a52'),
+		name: 'Your Socratic Buddy',
+		author: {
+			name: 'TOM GALLY',
+			url: ''
+		},
+		description: 'Thoughtful discussions with a knowledgeable partner',
+		tags: ['philosophy', 'discussion', 'conversation'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-o03gktdyW-your-socratic-buddy',
+		image:
+			'https://files.oaiusercontent.com/file-JfnzO7n2gPGaw9PHvFoBs1u4?se=2123-10-16T13%3A30%3A00Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Da05973f9-1b0f-4ef1-adc4-ede0df30125f.png&sig=ZwwFiiVUh8DvJY18/NsePn09tOnS8ugPLvhgJ4d3cew%3D'
+	},
+
+	{
+		id: isUuid('200eb415-0117-4a72-9677-3bc57661c2de'),
+		name: 'Permaculture GPT',
+		author: {
+			name: 'Anthony McLaughlin',
+			url: ''
+		},
+		description: 'Thoughtful discussions with a knowledgeable partner',
+		tags: ['permaculture', 'gardening', 'farming', 'sustainability', 'ecology', 'agriculture'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-y8BV6typ7-permaculture-gpt',
+		image:
+			'https://files.oaiusercontent.com/file-JfnzO7n2gPGaw9PHvFoBs1u4?se=2123-10-16T13%3A30%3A00Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Da05973f9-1b0f-4ef1-adc4-ede0df30125f.png&sig=ZwwFiiVUh8DvJY18/NsePn09tOnS8ugPLvhgJ4d3cew%3D'
+	},
+
+	{
+		id: isUuid('abe4a5b3-806c-4357-bd36-427da0c46e4a'),
+		name: 'LimitlessGPT',
+		author: {
+			name: 'Naja Faysal',
+			url: ''
+		},
+		description: 'Concise Step-by-Step Strategist',
+		tags: ['strategy', 'business', 'marketing', 'planning', 'productivity', 'management'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-XUgnMbahO-limitlessgpt',
+		image:
+			'https://files.oaiusercontent.com/file-kmJxKAgJIAxtDvKBoKtgCX0L?se=2123-10-18T02%3A09%3A01Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3DDALL%25C2%25B7E%25202023-11-10%252009.33.34%2520-%2520A%2520modern%252C%2520futuristic%252C%2520and%2520minimalistic%2520logo%2520for%2520%2527Limitless%252C%2527%2520a%2520productivity%2520and%2520life-planning%2520system.%2520The%2520logo%2520should%2520combine%2520symbols%2520of%2520a%2520brain%2520and%2520a.png&sig=fSSKOktPJSy0J67un1D5j8E711XhZGjqAuxmWBjC1Zo%3D'
+	},
+
+	{
+		id: isUuid('7d377bc1-22d4-4428-98ea-101965a6b914'),
+		name: 'SEO GOAT',
+		author: {
+			name: 'benjamin ogden',
+			url: ''
+		},
+		description: 'AI blog writer with SEO expertise and automatic AI image generation',
+		tags: ['seo', 'marketing', 'blogging', 'writing', 'content writing', 'images', 'pictures'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-vCVZafMQ3-seo-goat',
+		image:
+			'https://files.oaiusercontent.com/file-0nJY9hPmCTEwNS4yLDQtDkSG?se=2123-10-16T10%3A38%3A23Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dd2b70ac6-3476-494a-ad85-fa0bb3701f78.png&sig=rt1ZULrH%2BVsHiIiuoGHLEabhUzrODZClRbRHVHDzS5s%3D'
+	},
+
+	{
+		id: isUuid('201686b3-b454-4dbe-ad95-c7c1cbe4b0e4'),
+		name: 'DoubleSpeak GPT',
+		author: {
+			name: 'Safa Bilici',
+			url: ''
+		},
+		description: 'not telling lies whilst not telling the truth',
+		tags: ['politics', 'media', 'propaganda', 'marketing', 'business'],
+		added: new Date('2023-11-23').toISOString(),
+		updated: new Date('2023-11-23').toISOString(),
+		url: 'https://chat.openai.com/g/g-FEPMHxDYx-doublespeak-gpt',
+		image:
+			'https://files.oaiusercontent.com/file-Hm9eNsZ7QJvv9cFjwiWIKZ7Q?se=2123-10-22T19%3A31%3A58Z&sp=r&sv=2021-08-06&sr=b&rscc=max-age%3D31536000%2C%20immutable&rscd=attachment%3B%20filename%3Dd00a980f-d3f8-48b4-8169-7f98863ba1e2.png&sig=tFd5kd4XOjhuYTSybTkxZq1uPu5LpMl7PlOf1iC5TwI%3D'
 	}
 ];
