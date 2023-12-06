34a221fee1daca19a77ce41876852fddaf1ba123
Konstantin Kovar <mail@vomkonstant.in>
2023-12-06 14:10:30 +0100

    new build

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png","favicon16.png","logo1024.png","logo256.png","logo512.png","prompt_dress_logo.png","robots.txt"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.40b70114.js","app":"_app/immutable/entry/app.16b0d5bd.js","imports":["_app/immutable/entry/start.40b70114.js","_app/immutable/chunks/scheduler.ddf07609.js","_app/immutable/chunks/singletons.2becec3c.js","_app/immutable/chunks/index.24ec2bc8.js","_app/immutable/entry/app.16b0d5bd.js","_app/immutable/chunks/scheduler.ddf07609.js","_app/immutable/chunks/index.e55c1a00.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.6be89834.js","app":"_app/immutable/entry/app.32af46cd.js","imports":["_app/immutable/entry/start.6be89834.js","_app/immutable/chunks/scheduler.ddf07609.js","_app/immutable/chunks/singletons.86f5ea25.js","_app/immutable/chunks/index.24ec2bc8.js","_app/immutable/entry/app.32af46cd.js","_app/immutable/chunks/scheduler.ddf07609.js","_app/immutable/chunks/index.e55c1a00.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js')),
