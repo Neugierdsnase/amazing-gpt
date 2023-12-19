111689f5260f0b6f7e8cb0b524d2d0fbdc611335
Konstantin Kovar <mail@vomkonstant.in>
2023-12-19 10:03:04 +0100

    rating is pretty now

--- src/lib/components/CuratorsRating/CuratorsRating.svelte
+++ src/lib/components/CuratorsRating/CuratorsRating.svelte
@@ -1,5 +1,29 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { elasticOut } from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
+
 	export let rating: number;
+
+	let animatedRating = tweened(0.0, {
+		duration: 2200,
+		easing: elasticOut
+	});
+
+	onMount(() => {
+		animatedRating.set(Number(rating));
+	});
 </script>
 
-<span>{rating}/5</span>
+<div
+	style="background: linear-gradient(90deg,rgba(253,224,71,1)  0%, rgba(253,224,71,1) {$animatedRating *
+		13.6}%, oklch(var(--b1)) {$animatedRating * 14.2}%, oklch(var(--b1)) 100%);"
+	class="relative bg-yellow-300 flex text-md text-base-100 items-center px-2 py-1 rounded-lg overflow-hidden border-yellow-300 border-2"
+>
+	<i class="ph-fill ph-star" />
+	<i class="ph-fill ph-star" />
+	<i class="ph-fill ph-star" />
+	<i class="ph-fill ph-star" />
+	<i class="ph-fill ph-star" />
+	<span class="text-xs ml-2 mt-1 text-yellow-300"><b>{rating}</b>/5</span>
+</div>
